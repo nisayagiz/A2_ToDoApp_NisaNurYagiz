6f76c5fc217eff6bb8f80f55df237d5880ff3ce4
nisayagiz <yagiznisanur0@gmail.com>
2021-06-18 15:59:22 +0300

    this one does not properly wok

--- ToDoApp_NisaNurYagiz/Models/SearchViewModel.cs
+++ ToDoApp_NisaNurYagiz/Models/SearchViewModel.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using ToDoApp_NisaNurYagiz.Models;
 
 namespace ToDoApp_NisaNurYagiz.Models
 {
     public class SearchViewModel
     {
         public string SearchText { get; set; }
+
         public bool ShowAll { get; set; }
 
         public List<ToDo> Result { get; set; }
